03b1a5e9314e65d093c60b579d7b09de942fb4dc
Josue <javillasantet@gmail.com>
2016-10-25 22:39:44 -0500

    gitignore is now complete

--- guardarArchivo.js
+++ guardarArchivo.js
@@ -1,5 +1,5 @@
 function RegistrarArchivo() {
-    frames.ifr.document.getElementById('hdlId_Registro').value = '108';
+    frames.ifr.document.getElementById('hdlId_Registro').value = idPropuesta;
     frames.ifr.document.getElementById('hdlFl_Tipo').value = "1";
     frames.ifr.document.getElementById('btnValidar').click();
     return;
