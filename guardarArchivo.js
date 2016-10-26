f4a0d5d2560477ff70d51ca0e2fd022c63ecdcac
Renzo Burga <rburga.pe@gmail.com>
2016-10-26 13:21:49 -0500

    gg

--- guardarArchivo.js
+++ guardarArchivo.js
@@ -2,6 +2,9 @@ function RegistrarArchivo() {
     frames.ifr.document.getElementById('hdlId_Registro').value = idPropuesta;
     frames.ifr.document.getElementById('hdlFl_Tipo').value = "1";
     frames.ifr.document.getElementById('btnValidar').click();
+    frames.ifr.Materialize.updateTextFields();
+    $('#contenedor').hide();
+    $('#exito').removeClass('hide');
     return;
 }
 
